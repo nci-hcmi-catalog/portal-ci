09755f0f1153341374e95abd2460a0fdc0febcf0
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-08-29 17:29:34 -0400

    Excludes doc files from UI build

--- build_stage/build.sh
+++ build_stage/build.sh
@@ -4,7 +4,7 @@ export tag_version=`git tag -l --points-at HEAD`
 rm $2.tar 2>/dev/null
 
 if [ $2 = 'ui' ]; then
-  tar --exclude='./'$1 --exclude='./.git' --exclude='./Jenkinsfile' --exclude='./docs' --exclude='./data_model' --exclude='./cms.tar' --exclude='./api.tar' -cvf $2.tar .
+  tar --exclude='./'$1 --exclude='./.git' --exclude='./Jenkinsfile' --exclude='./docs' --exclude='./data_model' --exclude='./*.tar' --exclude='./README.md' --exclude='./README.md' -cvf $2.tar .
 else
   tar --exclude='./'$1 --exclude='./.git' --exclude='./Jenkinsfile' -cvf $2.tar ./$2
 fi

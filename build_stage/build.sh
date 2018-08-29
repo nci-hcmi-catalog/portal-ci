6c9d8a43e22b1d6866bda6624da2b4b35071fbb2
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-08-29 17:01:51 -0400

    Fixes UI build issue
    Builds UI from the root of the repository as UI uses other yarn workspaces in the repo.

--- build_stage/build.sh
+++ build_stage/build.sh
@@ -2,4 +2,9 @@
 echo "Building $2 package."
 export tag_version=`git tag -l --points-at HEAD`
 rm $2.tar 2>/dev/null
-tar --exclude='./'$1 --exclude='./.git' --exclude='./Jenkinsfile' -cvf $2.tar ./$2
+
+if [ $2 == 'ui' ]; then
+  tar --exclude='./'$1 --exclude='./.git' --exclude='./Jenkinsfile' -cvf $2.tar .
+else
+  tar --exclude='./'$1 --exclude='./.git' --exclude='./Jenkinsfile' -cvf $2.tar ./$2
+fi

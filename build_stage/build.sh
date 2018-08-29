2ef4a7f6a32c66d6b3cef166ee9dfeec07693ea8
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-08-29 17:09:55 -0400

    Fixes typo

--- build_stage/build.sh
+++ build_stage/build.sh
@@ -3,7 +3,7 @@ echo "Building $2 package."
 export tag_version=`git tag -l --points-at HEAD`
 rm $2.tar 2>/dev/null
 
-if [ $2 == 'ui' ]; then
+if [ "$2" == 'ui' ]; then
   tar --exclude='./'$1 --exclude='./.git' --exclude='./Jenkinsfile' -cvf $2.tar .
 else
   tar --exclude='./'$1 --exclude='./.git' --exclude='./Jenkinsfile' -cvf $2.tar ./$2
